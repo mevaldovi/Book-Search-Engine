69fa379fd565d7d17802f186ab8fadb1d6d12ea7
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-08-29 22:08:59 -0500

    debugged App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
+
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: "/graphql",
