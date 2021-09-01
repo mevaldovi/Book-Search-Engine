f9a81c885073c24ec5d6ac971920e40054565a9a
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-09-01 18:34:46 -0500

    changed line 5 in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,42 +2,44 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
+    _id: String
     username: String
     email: String
     password: String
     bookCount: Int!
     savedBooks: [Book]! 
-  }
+  }`;
+//on line 5, change 'String' back to 'ID' if your finding that its throwing errors. should be fine though.
 
-  type Book {
-    bookId: String
-    authors: [String]!
-    description: String
-    title: String
-    image: String
-    link: String
-  }
 
-  type Auth {
-    token: ID!
-    user: User
-  }
+//   type Book {
+//     bookId: String
+//     authors: [String]!
+//     description: String
+//     title: String
+//     image: String
+//     link: String
+//   }
 
-  type Query {
-    users: [User]
-    user(username: String!): User
-    books(username: String): [Book]
-    book(bookId: ID!): Book
-    me: User
-  }
+//   type Auth {
+//     token: ID!
+//     user: User
+//   }
 
-  type Mutation {
-    addUser(username: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
-    saveBook(authors: [String]!, description: String, title: String, image: String, link: String): User
-    removeBook(bookId: String!): User
-  }
-`;
+//   type Query {
+//     users: [User]
+//     user(username: String!): User
+//     books(username: String): [Book]
+//     book(bookId: ID!): Book
+//     me: User
+//   }
+
+//   type Mutation {
+//     addUser(username: String!, email: String!, password: String!): Auth
+//     login(email: String!, password: String!): Auth
+//     saveBook(authors: [String]!, description: String, title: String, image: String, link: String): User
+//     removeBook(bookId: String!): User
+//   }
+// `;
 
 module.exports = typeDefs;
